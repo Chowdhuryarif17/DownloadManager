3e87588a3ece2b52aeec8ba9b41b498fc84cc1e8
Ariful Haque Chowdhury <30564856+Chowdhuryarif17@users.noreply.github.com>
2020-11-13 16:42:17 +0600

    Disign Layout XML
    an EditText , A button and a progressBar

--- app/src/main/java/com/ariful/downloadmanager/MainActivity.java
+++ app/src/main/java/com/ariful/downloadmanager/MainActivity.java
@@ -3,12 +3,35 @@ package com.ariful.downloadmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText urlEt;
+    Button dowloadButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize_Views_With_XML();
+
+        dowloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+    private void initialize_Views_With_XML(){
+        urlEt = findViewById(R.id.urlEtId);
+        dowloadButton = findViewById(R.id.downloadUrlButtonId);
+        progressBar = findViewById(R.id.progressBarId);
     }
 }
